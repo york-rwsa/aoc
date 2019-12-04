dafda64c15187af5473fbbc97726da16b1242977
Rory Anderson <rory@jragon.co.uk>
2019-12-04 14:42:03 +0000

    day 4

---
+++ day4/day4_1.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+r = [153517, 630395]
+
+def check_order(x):
+  s = str(x)
+  return ''.join(sorted(s)) == s
+
+def has_double(x):
+  s = str(x)
+  count = Counter(s)
+
+  return any([v >= 2 for v in count.values()])
+
+val = 0
+for i in range(r[0], r[1] + 1):
+  if check_order(i):
+    if has_double(i):
+      val += 1
+
+print(val)
